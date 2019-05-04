f64aba3d4b3695ab2c2452ced91cb533b061e402
raedyalhallaq <raedalhallaq97@gmail.com>
2019-05-04 21:36:58 +0300

    supplier

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -6,7 +6,7 @@ Author: KeenThemes
 Website: http://www.keenthemes.com/
 Contact: support@keenthemes.com
 Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes 
 Like: www.facebook.com/keenthemes
 Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
 Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
